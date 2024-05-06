c7689f48414572758ed5fb34e5661b1c7d7f12de
Alexey Vlasov <alexey.vlasov@cosmos2.ru>
2024-05-06 16:27:25 +0300

    added camera, flash, rear camera functionality

--- lib/screens/camera/camera_screen.dart
+++ lib/screens/camera/camera_screen.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:ocr_cosmos2/screens/camera/camera.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
@@ -8,13 +10,38 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  List<CameraDescription>? cameras; // Сделали nullable
+  late Future<void> _initCameras;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCameras = initializeCameras();
+  }
+
+  Future<void> initializeCameras() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras(); // Присвоили значение переменной cameras
+    setState(() {}); // Обновляем UI после получения списка камер
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: const Text('Экран камеры'),
-      ),
+    return FutureBuilder<void>(
+      future: _initCameras,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            cameras != null) {
+          // Проверяем, что cameras не равно null
+          return Camera(
+            cameras: cameras!,
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
